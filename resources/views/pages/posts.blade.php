7aea3cc5253208622bd36b192d6a339399cd8809
Maksim_Alekseev <maxim040597@gmail.com>
2023-07-25 18:38:48 +0300

    отправка

--- resources/views/pages/posts.blade.php
+++ resources/views/pages/posts.blade.php
@@ -2,21 +2,20 @@
 
 @section('content')
 <div class="container container-240">
-    <h1>POST's</h1>
     @if(Session::has('error'))
         <p>{{ Session::get('error') }}</p>
     @endif
     @if(Session::has('success'))
         <p>{{ Session::get('success') }}</p>
     @endif
-    <form action="/obr" method="POST">
+    <!-- <form action="/obr" method="POST">
         <input type="text" name="field">
         <button>Отправить</button>
-    </form>
+    </form> -->
 </div>
 <div class="container container-240">
-    <form action="/obr" method="POST">
-        <h1>Регистрация</h1>
+    <form action="/obr" method="POST" class="two">
+        <h1>Registration</h1>
         <div class="name_reg ">
             <div class="name_item">
                 <label for="first_name">Имя:</label>
@@ -70,36 +69,9 @@
 
                     <select name="age" class="/#/">
                         <option value="" selected disabled>Год</option>
-                        <option value="1991">1991</option>
-                        <option value="1992">1992</option>
-                        <option value="1993">1993</option>
-                        <option value="1994">1994</option>
-                        <option value="1995">1995</option>
-                        <option value="1996">1996</option>
-                        <option value="1997">1997</option>
-                        <option value="1998">1998</option>
-                        <option value="1999">1999</option>
-                        <option value="2000">2000</option>
-                        <option value="2001">2001</option>
-                        <option value="2002">2002</option>
-                        <option value="2003">2003</option>
-                        <option value="2004">2004</option>
-                        <option value="2005">2005</option>
-                        <option value="2006">2006</option>
-                        <option value="2007">2007</option>
-                        <option value="2008">2008</option>
-                        <option value="2009">2009</option>
-                        <option value="2010">2010</option>
-                        <option value="2011">2011</option>
-                        <option value="2012">2012</option>
-                        <option value="2013">2013</option>
-                        <option value="2014">2014</option>
-                        <option value="2015">2015</option>
-                        <option value="2016">2016</option>
-                        <option value="2017">2017</option>
-                        <option value="2018">2018</option>
-                        <option value="2019">2019</option>
-                        <option value="2020">2020</option>
+                         @for ( $i = 2023; $i >= 1900; $i--)
+                            <option value="{{ $i }}">{{ $i }}</option>
+                        @endfor
                     </select>
                 </div>  
             </div>

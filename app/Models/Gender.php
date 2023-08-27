7eeea9a5b41f770bb390da2cb4df8a6e2d8f0d84
Maksim_Alekseev <maxim040597@gmail.com>
2023-08-28 01:50:48 +0300

    Fixed SQL - Gender

--- app/Models/Gender.php
+++ app/Models/Gender.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Gender extends Model
 {
     use HasFactory;
+
+    protected $table = 'genders';
     public function author()
     {
         return $this->hasOne(Author::class);  //(Post::class, 'author_id'); подключение явной связью (1 к 1)
